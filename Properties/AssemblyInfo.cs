61551444348502ecf23673718b7e377ac29aa2c1
Giovanni Manunta <giovanni.manunta@georgfischer.com>
2026-06-22 13:08:39 +0200

    Assembly Info Fixes

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GM.XrmToolBox.UnifiedTranslatorManager")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("XrmToolBox plugin to manage Dataverse / Dynamics 365 metadata translations")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Giovanni Manunta")]
 [assembly: AssemblyProduct("GM.XrmToolBox.UnifiedTranslatorManager")]
-[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyCopyright("Giovanni Manunta")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
